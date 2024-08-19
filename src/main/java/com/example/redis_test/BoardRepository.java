189f406432a2b95a05df63b99e46a562d5366021
hyukjunkim1116 <nmkk1234@naver.com>
2024-08-19 10:58:13 +0900

    first commit

---
+++ src/main/java/com/example/redis_test/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.example.redis_test;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Page<Board> findAllByOrderByCreatedAtDesc(Pageable pageable);
+}
